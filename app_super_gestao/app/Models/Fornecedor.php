dfa4eeac189a05facda62f06c26816f6a73abdf4
lademir <lademirjunior@hotmail.com>
2022-02-16 11:47:23 -0300

    eloquent e tinker

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// fornecedors -> padrao do laravel é pegar o nome do model e colocar um s no final
+
 class Fornecedor extends Model
 {
     use HasFactory;
+    // vai sobrepor a nomeacao automatica da tabela 
+    protected $table = 'fornecedores';
+    // permitir que sejam preenchidos a partir do metodo create
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
--- app_super_gestao/app/Models/SiteContato.php
+++ app_super_gestao/app/Models/SiteContato.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Site_Contato
+// site_contato
+// site_contatos
+
 class SiteContato extends Model
 {
     use HasFactory;
